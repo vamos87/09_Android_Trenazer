e3583e112d2e72cbc1088d8f21feb3617bfa3081
vamos87 <adrian.lawrentjew@gmail.com>
2019-03-05 20:53:53 +0100

    final commit

--- app/src/main/java/com/example/trenazer/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/trenazer/WorkoutDetailFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class WorkoutDetailFragment extends Fragment {
@@ -17,7 +18,7 @@ public class WorkoutDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public void setWorkoutId(long workoutId) {
+    public void setWorkout(long workoutId) {
         this.workoutId = workoutId;
     }
 
@@ -28,4 +29,16 @@ public class WorkoutDetailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if(view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText((workout.getName()));
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
 }
